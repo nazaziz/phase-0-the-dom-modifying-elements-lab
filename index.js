53219b2cb07a63980d599c4b21e950bf6549537e
naz <nazibrahimaziz@gmail.com>
2022-08-19 19:35:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here!
+
+const newP= document.createElement("p");
+const newT = document.createTextNode("Hi there and greetings!");
+document.body.appendChild(newT);
+console.log(newP);
+
+const ul = document.createElement("ul");
+for (let i = 0; i < 9; i++) {
+    const li = document.createElement("li");
+    li.textContent=(1+ i).toString();
+    ul.append(li);
+
+
+ }
+  document.body.append(ul);
+
+
+  const main =document.getElementById("main")
+  main.innerHTML=
+   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+main.style.height="300px";
+main.style.backgroundColor = "#ff9f9f";
+
+main.classList.add("naz");
+main.classList.remove("dog");
+
+const ul_1 = document.getElementsByTagName("ul")[0];
+const secondChild = ul_1.querySelector("li:nth-child(2)");
+const Child_9 =  ul_1.querySelector("li:nth-child(5)");
+ul.removeChild(secondChild);
+ul.removeChild(Child_9);
+
+ul.remove();
